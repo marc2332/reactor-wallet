963469997c478709758be301ec9110d008ec02c3
marc2332 <mespinsanz@gmail.com>
2022-03-23 17:10:59 +0100

    :bug: Fixed Unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,14 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reactor_wallet/main.dart';
-import 'package:reactor_wallet/pages/account_selection.dart';
 import 'package:reactor_wallet/pages/create_wallet.dart';
 import 'package:reactor_wallet/pages/home.dart';
 import 'package:reactor_wallet/pages/import_wallet.dart';
 import 'package:reactor_wallet/pages/watch_address.dart';
+import 'package:reactor_wallet/pages/welcome.dart';
 import 'package:reactor_wallet/utils/states.dart';
 
 final appLoadedProviderTesting = StateProvider<bool>((_) {
@@ -19,34 +18,26 @@ final appLoadedProviderTesting = StateProvider<bool>((_) {
 void main() {
   group('Navigation', () {
     ProviderScope app = ProviderScope(
-      child: App(),
+      child: const App(),
       overrides: [appLoadedProvider.overrideWithProvider(appLoadedProviderTesting)],
     );
-    testWidgets('App opens AccountSelectionPage page by default', (WidgetTester tester) async {
+    testWidgets('App opens Welcome page by default', (WidgetTester tester) async {
       await tester.pumpWidget(app);
 
       expect(find.byType(HomePage), findsOneWidget);
 
       await tester.pumpAndSettle();
 
-      expect(find.byType(AccountSelectionPage), findsOneWidget);
-    });
-
-    testWidgets('WatchAddress page', (WidgetTester tester) async {
-      await tester.pumpWidget(app);
-      await tester.pumpAndSettle();
-
-      await tester.tap(find.byType(InkWell).first);
-      await tester.pumpAndSettle();
-
-      expect(find.byType(WatchAddress), findsOneWidget);
+      expect(find.byType(WelcomePage), findsOneWidget);
     });
 
     testWidgets('ImportWallet page', (WidgetTester tester) async {
       await tester.pumpWidget(app);
       await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton).first);
+      await tester.pumpAndSettle();
 
-      await tester.tap(find.byType(InkWell).at(1));
+      await tester.tap(find.byType(InkWell).first);
       await tester.pumpAndSettle();
 
       expect(find.byType(ImportWallet), findsOneWidget);
@@ -55,11 +46,25 @@ void main() {
     testWidgets('CreateWallet page', (WidgetTester tester) async {
       await tester.pumpWidget(app);
       await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton).first);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byType(InkWell).at(1));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(CreateWallet), findsOneWidget);
+    });
+
+    testWidgets('WatchAddress page', (WidgetTester tester) async {
+      await tester.pumpWidget(app);
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton).first);
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byType(InkWell).last);
       await tester.pumpAndSettle();
 
-      expect(find.byType(CreateWallet), findsOneWidget);
+      expect(find.byType(WatchAddress), findsOneWidget);
     });
   });
 }
